7bc43f8d60af421044ea9c8dd48d4b7239bb175a
khuslee <c21io1053@ufe.edu.mn>
2022-01-21 14:24:51 +0800

    css uildel hylbarchilsan

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 //дэлгэц
 var IuController = (function(){
 
+var Doclib ={
+    inputType : ".add__type",
+    inputDesc : ".add__description",
+    inputValue : ".add__value",
+    addBtn : ".add__btn"
+
+}
+return{
+    getValue : function(){
+        return{
+            type : document.querySelector(Doclib.inputType).value,
+            description : document.querySelector(Doclib.inputDesc).value,
+           value : document.querySelector(Doclib.inputValue).value
+        };
+    },
+    getDomString : function(){
+        return Doclib;
+    }
+}
 })();
 //санхүү
 var FinanceController = (function(){
@@ -9,11 +28,18 @@ var FinanceController = (function(){
 //программ холбох
 var AppController =(function(FinanceController,IuController){
 
-    var ctrlAddItem = function(){
-console.log('print');
-    }
+   
 
-    document.querySelector(".add__btn").addEventListener('click',function(){
+    var ctrlAddItem = function(){
+
+        
+
+console.log(IuController.getValue())
+    }
+var setUpEventListener = function(){
+    var Domm = IuController.getDomString();
+
+    document.querySelector(Domm.addBtn).addEventListener('click',function(){
         ctrlAddItem();
     });
 
@@ -22,5 +48,14 @@ console.log('print');
         ctrlAddItem();
         }
     });
+}
+return {
+    init : function(){
+        console.log('app called')
+        setUpEventListener();
+    }
+};
    
 })(FinanceController,IuController);
+
+AppController.init();
